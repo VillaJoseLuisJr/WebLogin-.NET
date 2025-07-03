1bdd919ede031d630d9362339f46a759fe5aded0
Junior <villa.joseluisjr@gmail.com>
2025-07-03 14:23:24 -0300

    Llegué hasta el minuto 9:20 del video

---
+++ WebLogin/Data/WebDBContext.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using WebLogin.Models;
+
+namespace WebLogin.Data{
+
+    public class WebDBContext : DbContext
+    {
+        public WebDBContext(DbContextOptions<WebDBContext> options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(tb =>
+            {
+                tb.HasKey(col => col.IdUsuario);
+                tb.Property(col => col.IdUsuario)
+                .UseMySqlIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+                tb.Property(col => col.NombreCompleto).HasMaxLength(50);
+                tb.Property(col => col.Correo).HasMaxLength(50);
+                tb.Property(col => col.Clave).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Usuario>().ToTable("Usuario");
+        }
+    }
+}
